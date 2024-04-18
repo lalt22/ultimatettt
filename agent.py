bcff7f167d67d4b4f0a371ac53ee047e67d2b42e
lalt22 <lalitha2seshadri@gmail.com>
2024-04-18 15:38:57 +1000

    works against lvl 2

--- agent.py
+++ agent.py
@@ -8,6 +8,7 @@ import socket
 import sys
 import numpy as np
 import typing
+import copy
 
 # a board cell can hold:
 #   0 - Empty
@@ -22,8 +23,8 @@ current_move = 1
 s = [".","X","O"]
 curr = 0 # this is the current board to play in
 
-MIN_EVAL = -1000000 #alpha
-MAX_EVAL =  1000000 #beta
+MIN_EVAL = -1000000 
+MAX_EVAL =  1000000 
 
 # print a row
 def print_board_row(bd, a, b, c, i, j, k):
@@ -49,63 +50,195 @@ def print_board(board):
 # choose a move to play
 def play():
     # print_board(boards)
-    print(f"Playing move in board: {curr}. Current move is {current_move}")
+    print(f"Playing move in board: {curr}.")
     # just play a random move for now
     # n = np.random.randint(1,9)
     # while boards[curr][n] != 0:
         # n = np.random.randint(1,9)
-    alphabeta(1, current_move, 1, curr, MIN_EVAL, MAX_EVAL, best_move)
-    return_val = best_move[current_move]
-    print(f"playing {return_val} in grid {curr}", )
-    print("Returning: ",  return_val)
-    place(curr, return_val, 1)
+    alphabeta(1, current_move, 0, curr, MIN_EVAL, MAX_EVAL, best_move)
+    print(f"Placing move: [{curr}]{best_move[current_move]}")
+    place(curr, best_move[current_move], 1)
     
-    return return_val
+    return best_move[current_move]
 
-def alphabeta( player, current_move, cell, board, alpha, beta, best_move ):
-    print(f"Testing player {player}, move [{board}][{best_move[cell]}], sub-grid {board}, alpha {alpha}, beta {beta}")
+# def alphabeta( player, current_move, move_depth, cell, board, alpha, beta, best_move ):
+#     print_board(boards)
+#     print(f"Testing player {player} making move [{board}][{cell}], alpha {alpha}, beta {beta} depth {move_depth}")
+    
+#     best_eval = MIN_EVAL
+
+    
+#     for temp_cell in range(1,10):
+#         if (boards[board][temp_cell] == 0):
+#             # If enemy wins, return a very low eval
+#             boards[board][temp_cell] = (2-player+1)
+#             if game_won(2-player+1):
+#                 print(f"Player {2-player+1} wins with this move: [{board}][{cell}]")
+#                 boards[board][temp_cell] = 0
+#                 return -1000 + cell
+#             # If we win, return a very high eval
+#             boards[board][temp_cell] = (player)
+#             if (game_won(player)):
+#                 print(f"Player {player} wins with this move: [{board}][{cell}]")
+#                 boards[board][temp_cell] = 0
+#                 return 1000 + cell
+#             boards[board][temp_cell] = 0
+
+#     this_move = 0
+#     if (move_depth < 5):
+#         for r in range(1,10):
+#             print(f"Trying square {r} in board {board}: {boards[board][r]}")
+#             if boards[board][r] == 0:
+#                 print("Testing empty cell: ", boards[board][r])
+#                 this_move = r
+#                 boards[board][this_move] = player
+#                 print(f"Set cell [{board}][{this_move}] to {boards[board][this_move]}\n")
+#                 this_eval = -alphabeta((2-player+1), current_move, move_depth+ 1, cell, r, -beta, -alpha, best_move)
+#                 print(f'Best Eval: {best_eval} This Eval: {this_eval}')
+#                 boards[board][this_move] = 0
+#                 print(f"Unset cell [{board}][{this_move}] to {boards[board][this_move]}")
+#                 if this_eval > best_eval:
+#                     best_move[current_move] = this_move
+#                     best_eval = this_eval
+#                     if best_eval > alpha:
+#                         alpha = best_eval
+#                         if alpha >= beta:
+#                             print(f"Cutoff for move [{board}][{best_move[current_move]}] by player {player}\n")
+#                             return (alpha)
+#     else:
+#         print("Depth reached")
+#         return (alpha)
+#     if this_move == 0:
+#         print(f"No legal moves for {player}\n")
+#         return (0)
+#     else:
+#         print(f"Alpha return for move [{board}][{best_move[current_move]}] by player {player}")
+#         return (alpha)
+
+def alphabeta(player, current_move, depth, board, alpha, beta, best_move):
+    if depth == 0:
+        print("NEW MOVE EVALUATION")
     print_board(boards)
+    print(f"Player {player} in board {board} at depth {depth}")
+
     best_eval = MIN_EVAL
 
-    # If enemy wins, return a very low eval
+
     if game_won(2-player+1):
-        print(f"Player {2-player+1} wins with this move: [{board}][{best_move[cell]}]")
-        return -1000 + cell
+        return -1000 + depth
+    
+    #If enemy is one away from winning in current cell, occupy the cell
+    last_cell = is_one_away(2-player+1, board)
+    if last_cell:
+        print(f"Player {player} Blocking triple at [{board}][{last_cell}]")
+        if not is_one_away(2-player+1, last_cell):
+            best_move[current_move] = last_cell
+            return 100000
+
 
     this_move = 0
-    for r in range(1,10):
-        print(f"Trying square {r} in board {board}: {boards[board][r]}")
-        if boards[board][r] == 0:
-            print("Testing empty cell: ", boards[board][r])
-            this_move = r
-            boards[board][this_move] = player
-            print(f"Set cell [{board}][{this_move}] to {boards[board][this_move]}\n")
-            if (cell < 9):
-                this_eval = -alphabeta((2-player+1), current_move, cell+1, r, -beta, -alpha, best_move)
-                print(f'Best Eval: {best_eval} This Eval: {this_eval}')
-                boards[board][this_move] = 0
-                print(f"Unset cell [{board}][{this_move}] to {boards[board][this_move]}")
+    if depth < 3:
+        for cell in range(1,10):
+            if boards[board][cell] == 0:
+
+                if is_one_away(2-player+1, cell):
+                    this_eval = MIN_EVAL
+
+                else:
+                    print(f"Trying move [{board}][{cell}] for player {player}. Best Eval so far: {best_eval}")
+                    this_move = cell
+                    boards[board][this_move] = player
+                    this_eval = -alphabeta(2-player+1, current_move+1, depth + 1, cell, -beta, -alpha, best_move)
+                    boards[board][this_move] = 0
+                print(f"Player {player}. Eval of move [{board}][{cell}]: {this_eval}. Best Eval: {best_eval}")
                 if this_eval > best_eval:
-                    best_move[current_move] = this_move
-                    best_eval = this_eval
+                    
+
+                    if depth == 0:
+                       
+                        best_move[current_move] = this_move
+                        best_eval = this_eval
+                        print(f"Setting best move for move {current_move} by player {player} to be [{board}][{this_move}]")
+
                     if best_eval > alpha:
                         alpha = best_eval
-                        if alpha >= beta:
-                            print("Cutoff\n")
+
+                        if alpha > beta:
                             return (alpha)
     if this_move == 0:
-        print(f"No legal moves for {player}\n")
-        return (0)
+        return 0
     else:
-        print(f"Alpha return for move [{board}][{best_move[cell]}] by player {player}")
-        return (alpha)
-    pass
+        return alpha
+
+
+# def alphabeta(player, depth, board, alpha, beta):
+#     if depth == 0:
+#         print("NEW MOVE EVALUATION")
+#     print_board(boards)
+#     print(f"Player {player} in board {board}")
+
+#     if game_won(1):
+#         return 10 - depth
+#     elif game_won(2):
+#         return depth - 10
+    
+#     moves = []
+#     if (depth < 3):
+#         for cell in range(1,10):
+#             print(f"Trying cell {cell} in board {board}")
+
+#             if boards[board][cell] == 0:
+#                 #Check if this allows enemy to make winning next move
+#                 if is_one_away(1, cell):
+#                     return 10 - depth
+#                 elif is_one_away(2, cell):
+#                     return depth - 10
+            
+#                 boards[board][cell] = player
+#                 move = alphabeta(2-player+1, depth+1, cell, alpha, beta)
+#                 boards[board][cell] = 0
+#                 if player == 1:
+#                     alpha = max(alpha, move)
+#                 else:
+#                     beta = min(beta, move)
+                
+#                 if alpha >= beta:
+#                     break
+
+#                 if depth == 0:
+#                     moves.append([(board, cell), move])
+#                 else:
+#                         moves.append(move)
+#     if not moves:
+#         return 0
+
+#     if depth == 0:
+#         return list(max(moves, key=lambda x: x[1])[0] if player == 1 else min(moves, key=lambda x: x[1])[0])
+#         # max(moves) if player == 1 else min(moves)
+#     else:
+#         return max(moves) if player == 1 else min(moves)
+
+
 
 # place a move in the global boards
 def place( board, num, player ):
     global curr
     curr = num
     boards[board][num] = player
+
+def is_one_away(p, bd):
+    if ((boards[bd] == p).sum() < 2):
+        return False
+    for cell in range(1, 10):
+        if boards[bd][cell] == 0:
+            # print(f"Seeing if [{bd}][{cell}] wins game for {p}")
+            boards[bd][cell] = p
+            if (game_won(p)):
+                boards[bd][cell] = 0
+                print(f"{p} can win if they make move [{bd}][{cell}]")
+                return cell
+            boards[bd][cell] = 0
+    return None
 
 def board_won(p: int, bd: int):
     return(  ( boards[bd][1] == p and boards[bd][2] == p and boards[bd][3] == p)
